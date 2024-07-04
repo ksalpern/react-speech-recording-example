c29c8b576d700d34943529cd0d6c772f761d7c00
Kseniia <ksenia.pidopryhora@gmail.com>
2024-07-04 18:54:14 +0300

    add basic app styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+export const App = () => {
+  return <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam consequatur modi reprehenderit quam? Iste reprehenderit animi quis aliquid a, reiciendis, cum commodi ducimus totam natus voluptate culpa, quibusdam excepturi sed.
+  Sunt ipsa sequi vitae voluptatem iusto, adipisci animi amet, dicta iure earum natus. Libero, modi repudiandae aut architecto omnis sint ea dolorem molestias, veniam aspernatur culpa reprehenderit obcaecati reiciendis neque?
+  Quidem placeat nesciunt harum ea, eius quia fuga doloribus sequi illum omnis iure amet iusto dolor voluptatum excepturi officia, quod cupiditate adipisci eligendi nam, ad animi voluptatem asperiores laborum. Dolores!
+  Numquam, voluptatem. Corporis nostrum, illo, ex ipsam ab explicabo molestiae est ducimus fugiat vero fugit! Doloremque nam dolorum temporibus illum velit debitis, quas vitae autem sequi consequatur modi molestiae a.
+  Quibusdam quam nihil fugit soluta aspernatur esse commodi eius aut vitae reiciendis rerum explicabo illum repudiandae consectetur ea quo voluptates corporis optio dolor voluptatem excepturi dignissimos perspiciatis, porro odit. Molestias.
+  Quo asperiores nobis voluptas nulla ipsam. Laboriosam nobis cumque mollitia possimus sit, provident doloribus ex, error minus voluptatem pariatur omnis a necessitatibus, rem nihil. Ipsa itaque explicabo rerum delectus ad!
+  Eveniet minus sint adipisci illo optio facilis ipsum recusandae dolorum? Assumenda, error temporibus? Saepe quisquam nihil corrupti eum nostrum maxime praesentium quod doloribus consectetur exercitationem! Necessitatibus nulla expedita ad illo?
+  Debitis est eum eos optio dolorum veniam minima tempore possimus eius recusandae quibusdam officiis odit nisi quis a maxime, libero assumenda, aliquid eveniet animi. Voluptatum sed nesciunt id suscipit repellat!
+  Sint, itaque. Obcaecati, deserunt delectus consectetur placeat esse laboriosam autem a, nostrum odio fuga ea veritatis sint. Dolore, illum? Cupiditate recusandae ipsa voluptas est, suscipit cum neque laudantium temporibus incidunt?
+  Illum pariatur atque velit perspiciatis eum, neque dolorem repudiandae recusandae, excepturi, quos architecto delectus est aperiam voluptatem consequuntur quae repellendus ullam quidem obcaecati molestiae nesciunt voluptates! Repellat quia a assumenda?</div>;
+};
